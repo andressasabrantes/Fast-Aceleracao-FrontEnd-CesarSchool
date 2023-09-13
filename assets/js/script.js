86582d4d4f895f6384035c9ffbfafe1242d51c0e
Andressa Abrantes <andressasabrantes@gmail.com>
2023-09-13 20:38:22 -0300

    Adicionando o javascript

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const contatos = [
+    {
+        nome: "Joaozinho",
+        telefone: "989876768",
+        email: "joaozinho@gmail.com",
+        interesses: ["Futevolei", "Praia", "Futebol"]
+    },
+    {
+        nome: "Maria",
+        telefone: "9989876768",
+        email: "maria@gmail.com",
+        interesses: ["Futevolei", "Praia", "Programação"]
+    }
+]
+
+// console.log(contatos)
+
+contatos.forEach((item) => console.log(item))
